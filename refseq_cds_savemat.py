1bce27d53201342e57860f80490a89f96d21939f
Dave Beck <dacb@uw.edu>
2019-05-09 14:41:15 -0700

    simplified script to only read and write

--- refseq_cds_savemat.py
+++ refseq_cds_savemat.py
@@ -8,27 +8,27 @@ import pandas as pd
 import scipy.io
 
 parser = argparse.ArgumentParser(description='convert refseq cds tsv to .mat file',
-            usage='e.g., ./refseq_cds_savemat.py family Enterobacteriaceae refseq_cds_matched.tsv refseq_cds_nonmatched.tsv')
+            usage='e.g., ./refseq_cds_savemat.py family Enterobacteriaceae refseq_cds_balanced.mat refseq_cds_balanced.tsv')
 parser.add_argument('taxlevel', 
         choices=['kingdom', 'phylum', 'class', 'order', 'family', 'genus'],
         help='what taxonomy level should be used as the class')
 parser.add_argument('taxa', 
         help='the taxonomy class that will be class 0')
-parser.add_argument('filenames', nargs='*',
-        help='the refseq cds tsv files to be concatenated and converted to .mat')
+parser.add_argument('infile',
+        help='the balanced refseq cds tsv file to be converted to .mat')
+parser.add_argument('outfile',
+        help='the balanced refseq cds .mat outfile file')
 args = parser.parse_args()
 taxlevel = args.taxlevel
 taxa = args.taxa
-filenames = args.filenames
+infile = args.infile
+outfile = args.outfile
 
-print(f"reading tsv data file {filenames[0]}")
-df = pd.read_csv(filenames.pop(0), sep='\t').filter(items=['sequence', taxlevel])
-for filename in filenames:
-    print(f"reading tsv data file {filename}")
-    df.append(pd.read_csv(filename, sep='\t').filter(items=['sequence', taxlevel]))
+print(f"reading tsv data file {infile}")
+df = pd.read_csv(infile, sep='\t').filter(items=['sequence', taxlevel])
 
 outfile = 'refseq_cds.mat'
 print(f"saving mat data to file {outfile}")
 target = np.array(df[taxlevel] == taxa, dtype=int)
 scipy.io.savemat(outfile, { 'sequence' : df['sequence'],
-        'target' : target}, appendmat = True)
+        'target' : target }, appendmat=True)
