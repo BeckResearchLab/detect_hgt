45f3237f3919aee2096181e2fa1c2979e99a5512
Dave Beck <dacb@uw.edu>
2019-05-10 14:55:17 -0700

    fixed up the filtering line with the new preparation protocol

--- refseq_prepare.sh
+++ refseq_prepare.sh
@@ -4,20 +4,14 @@ REFSEQ_PATH=/work/data/refseq
 THREADS=24
 MIN_SEQ_LEN=750
 
-if [ ! -e refseq_cds.tsv -o ]; then
+if [ ! -e refseq_cds.tsv ]; then
 	./refseq_cds_extractor.py --refseq_path $REFSEQ_PATH \
 			--output_file refseq_cds.tsv --threads $THREADS
 fi
 
-if [ ! -e refseq_cds_matched_filtered.tsv ]; then
-	./refseq_cds_filter.py refseq_cds_matched.tsv $MIN_SEQ_LEN refseq_cds_matched_filtered.tsv&
-fi
-
-if [ ! -e refseq_cds_not_matched_filtered.tsv ]; then
-	./refseq_cds_filter.py refseq_cds_not_matched.tsv $MIN_SEQ_LEN refseq_cds_not_matched_filtered.tsv&
-fi
-
-wait
+if [ ! -e refseq_cds_filtered.tsv ]; then
+	./refseq_cds_filter.py --input_file refseq_cds.tsv --output_file refseq_cds_filtered.tsv \
+			--min_seq_length $MIN_SEQ_LEN --trim_seq_length $MIN_SEQ_LEN
 
 if [ ! -e refseq_cds_balanced.tsv ]; then
 	./refseq_cds_balance.py family Enterobacteriaceae refseq_cds_balanced.tsv refseq_cds_matched_filtered.tsv refseq_cds_not_matched_filtered.tsv
