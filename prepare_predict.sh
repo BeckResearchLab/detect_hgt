107920098779289d51af4793fe98d6211653893a
Dave Beck <dacb@uw.edu>
2019-05-11 12:31:33 -0700

    fixed up myml for evaluation and prediction

--- prepare_predict.sh
+++ prepare_predict.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-awk -F'\t' '{ printf(">%s\t%s\n%s\n", $9, $7, $10); }' refseq_cds_filtered.tsv
+mkdir -f predict_sequences
+head -1000 refseq_cds_filtered.tsv | awk -F'\t' '{ printf(">%s\t%s\n%s\n", $9, $7, $10); }' > predict_sequences/seqs.fa
